fe88f3929ad472d94dfd69d901aabe3e618e74e0
Arieh Norton <arieh.norton@gmail.com>
2021-11-27 20:14:12 +0200

    NodeEdge object

---
+++ Ex2/src/NodeEdge.java
@@ -0,0 +1,51 @@
+import api.EdgeData;
+
+public class NodeEdge implements EdgeData {
+    private int src;
+    private int dest;
+    private double weight;
+    private String info;
+    private int tag;
+
+
+    public NodeEdge(int Src, double Weight, int Dest){
+        this.src = Src;
+        this.weight = Weight;
+        this. dest = Dest;
+    }
+
+    @Override
+    public int getSrc() {
+        return this.src;
+    }
+
+    @Override
+    public int getDest() {
+        return this.dest;
+    }
+
+    @Override
+    public double getWeight() {
+        return this.weight;
+    }
+
+    @Override
+    public String getInfo() {
+        return this.info;
+    }
+
+    @Override
+    public void setInfo(String s) {
+        this.info = s;
+    }
+
+    @Override
+    public int getTag() {
+        return this.tag;
+    }
+
+    @Override
+    public void setTag(int t) {
+        this.tag = t;
+    }
+}
