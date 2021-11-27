0ed4d4810d6a41ee739c24ff00cc5b5513da2e04
Arieh Norton <arieh.norton@gmail.com>
2021-11-27 19:57:44 +0200

    P

--- Ex2/src/Point3D.java
+++ Ex2/src/Point3D.java
@@ -10,23 +10,37 @@ public class Point3D implements GeoLocation {
         this._y = y;
         this._z = z;
     }
+
     @Override
+    // returning x value
     public double x() {
         return this._x;
     }
 
     @Override
+    // returning y value
     public double y() {
         return this._y;
     }
 
     @Override
+    // returning z value
     public double z() {
         return this._z;
     }
 
     @Override
+    // returning distance between two points
     public double distance(GeoLocation g) {
-        return 0;
+        /* the equation of the distance between two 3d points is
+        sqrt ((x2-x1)**+(y2-y1)**+(z2-z1)**)
+         */
+        double x_pow = Math.pow(g.x()-this.x(), 2);
+        double y_pow = Math.pow(g.y()-this.y(), 2);
+        double z_pow = Math.pow(g.z()-this.z(), 2);
+
+        return Math.sqrt(x_pow+y_pow+z_pow);
     }
+
+
 }
