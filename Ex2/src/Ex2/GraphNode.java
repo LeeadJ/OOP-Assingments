3c8509d4f4891325cea06e48325233c3f93a493f
Arieh Norton <arieh.norton@gmail.com>
2021-11-28 11:34:17 +0200

    change key to final

--- Ex2/src/Ex2/GraphNode.java
+++ Ex2/src/Ex2/GraphNode.java
@@ -4,7 +4,7 @@ import api.GeoLocation;
 import api.NodeData;
 
 public class GraphNode implements NodeData {
-    private int key;
+    private final int key;
     private GeoLocation location;
     private double weight;
     private String info;
